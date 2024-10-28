d42c4e0f209c11d32e96fef330f92d25a0a8c0c8
Johnatan Valencia <johnatanv91@gmail.com>
2024-10-28 15:46:52 -0500

    clase adminMateria agregada

--- backend/classes/materia.py
+++ backend/classes/materia.py
@@ -2,26 +2,25 @@ from backend.database import Database
 import psycopg2
 
 class Materia():
-    ident = ""
-    nombre = ""
-    facultad = ""
-    cedula = None
-    pregrado = ""
+    id_materia = ""
+    nombre_materia = ""
+    programa = ""
+    intensidad_horaria = ""
     
-    def __init__(self):
-        self.ident = None
-        self.nombre = None
-        self.facultad = None
-        self.cedula = None
-        self.pregrado = None
-
+    def _init_(self):
+        self.id_materia = None
+        self.nombre_materia = None
+        self.programa = None
+        self.intensidad_horaria = None
+        
+        #Configuración BD
         self.db = Database(dbname = "pooDATABASE" , user = "postgres", password = "poo1234", host = "localhost", port = 5432)
         self.conexion = self.db.conectar()
         
         if self.conexion:
             self.cursor = self.conexion.cursor() 
         else: 
-            print('Sin conexión')
+            print('Sin conexión a la base de datos')
             
         self.verificarCrearTablaMateria()
             
@@ -47,11 +46,10 @@ class Materia():
         try:
             query = """
             CREATE TABLE IF NOT EXISTS materia (
-                id SERIAL PRIMARY KEY,            -- ID auto-incrementable y clave primaria
-                nombre VARCHAR(255),     -- nombre
-                facultad VARCHAR(255),     -- facultad
-                cedula VARCHAR(255), -- cedula
-                pregrado VARCHAR(255), --pregrado
+                id_materia VARCHAR(10) PRIMARY KEY,  -- Identificador de la materia y clave primaria
+                nombre_materia VARCHAR(255),         -- nombre
+                programa VARCHAR(255),               -- facultad
+                intesidad_horaria INT                -- Instensidad horaria
             );
             """
             #consulta para crear la tabla
@@ -62,3 +60,65 @@ class Materia():
         except psycopg2.Error as e:
             self.conexion.rollback()
             print(f"Error al crear la tabla: {e}")
+
+class adminMateria(Materia):
+    def crearMateria (self, id_materia, nombre_materia, programa, intensidad_horaria):
+        if self.id_materia and self.nombre_materia and self.programa and self.intensidad_horaria:
+            try: 
+                query = """
+                INSERT INTO materias(id_materia, nombre_materia, programa, intensidad_horaria)
+                VALUES (%s, %s, %s, %s)
+                """
+            except psycopg2.Error as e:
+                self.conexion.rollback()
+                print(f"Error al crear la materia: {e}")
+                return False
+    def actualizarMateria (self, id_materia, nombre_materia, programa, intensidad_horaria):
+        try:
+            query = """
+            UPDATE materias SET
+                nombre_materia = %s,
+                programa = %s,
+                intensidad_horaria = %s,
+            WHERE id_materia = %s
+            """
+            values = (nombre_materia, programa, intensidad_horaria, id_materia)
+            self.cursor.execute(query, values)
+            self.conexion.commit()
+            print(f"Materia con ID{id_materia} actualizada correctamente")
+            return True
+        except psycopg2.Error as e:
+            self.conexion.rollback()
+            print(f"Error al actualizar la materia: {e}")
+            return False
+        
+    def eliminarMateria (self, id_materia):
+        try:
+            query = "DELETE FROM materias EHERE id_materia = %s"
+            self.cursor.execute(query, (id_materia,))
+            self.conexion.commit()
+            print(f"Materia con ID {id_materia} eliminada correctamente")
+            return True
+        except psycopg2.Error as e:
+            self.conexion.rollback()
+            print(f"Error al aeliminar la materia: {e}")
+            return False
+        
+    def cargarMaterias(self):
+        try:
+            query = "SELECT nombre_materia FROM materias"
+            self.cursor.execute(query)
+            materias = self.cursor.fetchall()
+            return [materia[0] for materia in materias]
+        except psycopg2.Error as e:
+            print(f"Error al cargar las materias: {e}")
+            return []
+    
+    def cargarDetallerMaterias (self, id_materia):
+        try:
+            query = "SELECT id_materia, nombre_materia, programa, intensidad_horaria FROM materias WHERE id_materia = %s"
+            self.cursor.execute(query, (id_materia,))
+            return self.cursor.fetchone() 
+        except psycopg2.Error as e:
+            print(f"Error al cargar detalles de la materia: {e}")
+            return None
